97bbc2c40729dc7ded5f084e15f1c1850bf1d5a0
YejeongKang <dpwjd1130@gmail.com>
2024-06-13 16:32:48 +0900

    m_roll_try 미사용으로 폐기, round 보이기

--- YachtDicePrototype2/YachtDice1Dlg.h
+++ YachtDicePrototype2/YachtDice1Dlg.h
@@ -92,8 +92,7 @@ public:
 	afx_msg void OnBnClickedDiceButton11();
 	afx_msg void OnBnClickedChoosecategory();
 
-	CStatic m_roll_try;
-	int m_round;
+	int m_roll, m_round;
 	vector<bool> pickDice;
 	vector<int> dice_result;
 
@@ -118,4 +117,5 @@ public:
 	afx_msg void OnBnClickedP1_10();
 	afx_msg void OnBnClickedP1_11();
 	afx_msg void OnBnClickedP1_12();
+	CStatic m_round_num;
 };
