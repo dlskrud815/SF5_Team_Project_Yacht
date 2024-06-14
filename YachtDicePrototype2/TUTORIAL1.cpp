9438b083f3c408aac60c4d6248f407cbfc2c5c9f
dlskrud815 <dlskrud815@gmail.com>
2024-06-14 20:41:40 +0900

    튜토리얼 RULE 페이지 배경 적용

--- YachtDicePrototype2/TUTORIAL1.cpp
+++ YachtDicePrototype2/TUTORIAL1.cpp
@@ -57,7 +57,7 @@ void TUTORIAL1::OnInitialUpdate()
 	CFormView::OnInitialUpdate();
 
 	// TODO: Add your specialized code here and/or call the base class
-	back.Load(_T("BACKGROUND.png"));//이미지 로드
+	back.Load(_T("TUTORIAL_RULE.png"));//이미지 로드
 	m_next_btn.LoadBitmaps(IDB_NEXT, NULL, NULL, NULL); // 첫 번째 인자에는 추가한 비트맵 ID명
 	m_next_btn.SizeToContent(); // 이미지 크기에 버튼 크기를 맞춰주는 작업
 }
